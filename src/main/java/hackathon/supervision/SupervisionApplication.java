bc19d6275754c267c9b7b1877cd32e8fcf1ee5a0
szajbot <tobiasz.waszkowiak00@gmail.com>
2022-10-22 19:04:11 +0200

    initial-commit

---
+++ src/main/java/hackathon/supervision/SupervisionApplication.java
@@ -0,0 +1,13 @@
+package hackathon.supervision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupervisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hackathon/supervision/SupervisionApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.supervision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
